76e2fdbe3aa32a861b9445c9c752a56481d86a4c
SyaoranFox <syaoranfox@gmail.com>
2025-12-18 19:34:31 +1100

    Current changes to bring Delta up to Consent v1, has database error.

--- Content.Client/Input/ContentContexts.cs
+++ Content.Client/Input/ContentContexts.cs
@@ -145,6 +145,7 @@ namespace Content.Client.Input
             common.AddFunction(ContentKeyFunctions.OpenDecalSpawnWindow);
             common.AddFunction(ContentKeyFunctions.OpenAdminMenu);
             common.AddFunction(ContentKeyFunctions.OpenGuidebook);
+            common.AddFunction(ContentKeyFunctions.OpenConsentWindow);
         }
     }
 }
--- Content.Server/IoC/ServerContentIoC.cs
+++ Content.Server/IoC/ServerContentIoC.cs
@@ -5,6 +5,7 @@ using Content.Server.Administration.Notes;
 using Content.Server.Afk;
 using Content.Server.Chat.Managers;
 using Content.Server.Connection;
+using Content.Server.Consent;
 using Content.Server.Database;
 using Content.Server._DV.FeedbackPopup; // DeltaV
 using Content.Server.Discord;
@@ -81,6 +82,7 @@ namespace Content.Server.IoC
 
             IoCManager.Register<DiscordLink>();
             IoCManager.Register<DiscordChatLink>();
+            IoCManager.Register<IServerConsentManager, ServerConsentManager>();
         }
     }
 }
