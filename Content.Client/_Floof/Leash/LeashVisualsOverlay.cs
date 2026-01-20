476747e4c86583d7bf49e0d4f867d22d5c7b206d
Mnemotechnician <69920617+mnemotechnician@users.noreply.github.com>
2026-01-20 18:57:14 +0300

    Leashes

---
+++ Content.Client/_Floof/Leash/LeashVisualsOverlay.cs
@@ -0,0 +1,111 @@
+using System.Numerics;
+using Content.Shared._Floof.Paint;
+using Content.Shared.Floofstation.Leash.Components;
+using Robust.Client.GameObjects;
+using Robust.Client.Graphics;
+using Robust.Shared.Enums;
+
+namespace Content.Client._Floof.Leash;
+
+public sealed class LeashVisualsOverlay : Overlay
+{
+    public override OverlaySpace Space => OverlaySpace.WorldSpaceBelowFOV;
+
+    private readonly IEntityManager _entMan;
+    private readonly SpriteSystem _sprites;
+    private readonly SharedTransformSystem _xform;
+
+    private readonly EntityQuery<TransformComponent> _xformQuery;
+    private readonly EntityQuery<SpriteComponent> _spriteQuery;
+    private readonly EntityQuery<ColorPaintedComponent> _paintQuery;
+
+    public LeashVisualsOverlay(IEntityManager entMan)
+    {
+        _entMan = entMan;
+        _sprites = _entMan.System<SpriteSystem>();
+        _xform = _entMan.System<SharedTransformSystem>();
+        _xformQuery = _entMan.GetEntityQuery<TransformComponent>();
+        _spriteQuery = _entMan.GetEntityQuery<SpriteComponent>();
+        _paintQuery = _entMan.GetEntityQuery<ColorPaintedComponent>();
+    }
+
+    protected override void Draw(in OverlayDrawArgs args)
+    {
+        var worldHandle = args.WorldHandle;
+        worldHandle.SetTransform(Vector2.Zero, Angle.Zero);
+
+        var query = _entMan.EntityQueryEnumerator<LeashedVisualsComponent>();
+        while (query.MoveNext(out var visualsComp))
+        {
+            if (visualsComp.Source is not {Valid: true} source
+                || visualsComp.Target is not {Valid: true} target
+                || !_xformQuery.TryGetComponent(source, out var xformComp)
+                || !_xformQuery.TryGetComponent(target, out var otherXformComp)
+                || xformComp.MapID != args.MapId
+                || otherXformComp.MapID != xformComp.MapID)
+                continue;
+
+            var texture = _sprites.Frame0(visualsComp.Sprite);
+            var width = texture.Width / (float) EyeManager.PixelsPerMeter;
+
+            var coordsA = xformComp.Coordinates;
+            var coordsB = otherXformComp.Coordinates;
+
+            // If both coordinates are in the same spot (e.g. the leash is being held by the leashed), don't render anything
+            if (coordsA.TryDistance(_entMan, _xform, coordsB, out var dist) && dist < 0.01f)
+                continue;
+
+            var rotA = xformComp.LocalRotation;
+            var rotB = otherXformComp.LocalRotation;
+            var offsetA = visualsComp.OffsetSource;
+            var offsetB = visualsComp.OffsetTarget;
+
+            // NoRotation sprites always have a zero rotation, and their "up" is always facing the viewport "up"
+            // Regular sprites on the other hand can have any rotation, and their rotation is described in world coordinates
+            if (_spriteQuery.TryGetComponent(source, out var spriteA))
+            {
+                offsetA *= spriteA.Scale;
+                offsetA += spriteA.Offset;
+                if (spriteA.NoRotation)
+                    rotA = -args.Viewport.Eye?.Rotation ?? Angle.Zero;
+                else
+                    rotA = spriteA.Rotation;
+            }
+            if (_spriteQuery.TryGetComponent(target, out var spriteB))
+            {
+                offsetB *= spriteB.Scale;
+                offsetB += spriteB.Offset;
+                if (spriteB.NoRotation)
+                    rotB = -args.Viewport.Eye?.Rotation ?? Angle.Zero;
+                else
+                    rotB = spriteB.Rotation;
+            }
+
+            coordsA = coordsA.Offset(rotA.RotateVec(offsetA));
+            coordsB = coordsB.Offset(rotB.RotateVec(offsetB));
+
+            var posA = _xform.ToMapCoordinates(coordsA).Position;
+            var posB = _xform.ToMapCoordinates(coordsB).Position;
+            var diff = (posB - posA);
+            var length = diff.Length();
+
+            // So basically, we find the midpoint, then create a box that describes the sprite boundaries, then rotate it
+            var midPoint = diff / 2f + posA;
+            var angle = (posB - posA).ToWorldAngle();
+            var box = new Box2(-width / 2f, -length / 2f, width / 2f, length / 2f);
+            var rotate = new Box2Rotated(box.Translated(midPoint), angle, midPoint);
+
+            // Source is always the leash as of now.
+            // If it ever changes, make sure to change the visuals comp to include a reference to the leash.
+            var color = _paintQuery.CompOrNull(source)?.Color;
+
+            // We draw the leash as multiple segments
+            // First determine the maximum segment length and segment count based on the sprite height
+            var maxSegmentLength = texture.Height / (float)EyeManager.PixelsPerMeter;
+            int segmentCount = Math.Max(1, (int)Math.Ceiling(length / maxSegmentLength));
+
+            var direction = diff / length;
+            worldHandle.DrawTextureRect(texture, rotate, color);
+        }
+    }
+}
