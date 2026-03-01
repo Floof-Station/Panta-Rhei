547296e083c25f031124681bfccec365a7b925ef
Raikyr0 <Kurohana@hotmail.com.au>
2026-03-02 03:32:17 +1100

    Add files via upload
    Signed-off-by: Raikyr0 <Kurohana@hotmail.com.au>

---
+++ Content.Shared/_Floof/Traits/Effects/ChangePassiveHealingEffect.cs
@@ -0,0 +1,19 @@
+﻿using Content.Shared._DV.Traits.Effects;
+using Content.Shared.Damage;
+using Content.Shared.Damage.Components;
+
+namespace Content.Shared._Floof.Traits.Effects;
+
+public partial class ChangePassiveHealingEffect : BaseTraitEffect
+{
+    [DataField(required: true)] public DamageSpecifier AddedDamage = default!;
+
+    // I forgor the signature
+    public override void Apply(TraitEffectContext args)
+    {
+        if (!args.EntMan.TryGetComponent<PassiveDamageComponent>(args.Player, out var passiveDamage))
+            return;
+
+        passiveDamage.Damage += AddedDamage;
+    }
+}
