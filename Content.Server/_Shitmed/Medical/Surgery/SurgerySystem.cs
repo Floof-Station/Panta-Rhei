cdd2b175355b65e4991c073682f3a82a7eaeccf1
Raikyr0 <Kurohana@hotmail.com.au>
2026-03-06 04:02:01 +1100

    Update SurgerySystem.cs
    Signed-off-by: Raikyr0 <Kurohana@hotmail.com.au>

--- Content.Server/_Shitmed/Medical/Surgery/SurgerySystem.cs
+++ Content.Server/_Shitmed/Medical/Surgery/SurgerySystem.cs
@@ -236,11 +236,11 @@ public sealed class SurgerySystem : SharedSurgerySystem
 
     private void OnSurgerySpecialDamageChange(Entity<SurgerySpecialDamageChangeEffectComponent> ent, ref SurgeryStepDamageChangeEvent args)
     {
-        /* DeltaV - this shit was killed
+    
         // Im killing this shit soon too, inshallah.
         if (ent.Comp.DamageType == "Rot")
-             _rot.ReduceAccumulator(args.Body, TimeSpan.FromSeconds(2147483648)); // BEHOLD, SHITCODE THAT I JUST COPY PASTED. I'll redo it at some point, pinky swear :)
-        */
+             _rot.ReduceAccumulator(args.Body, TimeSpan.FromSeconds(2147483648)); // BEHOLD, SHITCODE THAT I JUST COPY PASTED. I'll redo it at some point, pinky swear :) 
+             
         /*else if (ent.Comp.DamageType == "Eye"
             && TryComp(ent, out BlindableComponent? blindComp)
             && blindComp.EyeDamage > 0)
