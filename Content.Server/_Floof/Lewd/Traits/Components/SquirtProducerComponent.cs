ef7451776091668cbe19ee18aee8a64b4e936d41
Raikyr0 <Kurohana@hotmail.com.au>
2026-03-14 11:20:41 +1100

    Update SquirtProducerComponent.cs
    Signed-off-by: Raikyr0 <Kurohana@hotmail.com.au>

--- Content.Server/_Floof/Lewd/Traits/Components/SquirtProducerComponent.cs
+++ Content.Server/_Floof/Lewd/Traits/Components/SquirtProducerComponent.cs
@@ -16,7 +16,7 @@ public sealed partial class SquirtProducerComponent : Component
     public ProtoId<ReagentPrototype> ReagentId = "NaturalLubricant";
 
     [DataField]
-    public FixedPoint2 MaxVolume = FixedPoint2.New(50);
+    public FixedPoint2 MaxVolume = FixedPoint2.New(30);
 
     [ViewVariables]
     public Entity<SolutionComponent>? Solution = null;
@@ -25,7 +25,7 @@ public sealed partial class SquirtProducerComponent : Component
     public FixedPoint2 QuantityPerUpdate = 5;
 
     [DataField]
-    public float HungerUsage = 5f;
+    public float HungerUsage = 10f;
 
     [DataField]
     public TimeSpan GrowthDelay = TimeSpan.FromSeconds(10);
