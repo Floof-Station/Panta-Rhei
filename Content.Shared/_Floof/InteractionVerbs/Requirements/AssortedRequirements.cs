dd6149fe704f3bf6660dc6ce0c27170d9092d84c
Mnemotechnician <69920617+mnemotechnician@users.noreply.github.com>
2026-04-13 11:40:52 +0300

    UnderwearRequirement and ConsentRequirement separation into separate files

--- Content.Shared/_Floof/InteractionVerbs/Requirements/AssortedRequirements.cs
+++ Content.Shared/_Floof/InteractionVerbs/Requirements/AssortedRequirements.cs
@@ -72,27 +72,3 @@ public sealed partial class SelfTargetRequirement : InvertableInteractionRequire
     }
 }
 
-/// <summary>
-///     Requires the target to consent to the action.
-/// </summary>
-[Serializable, NetSerializable]
-public sealed partial class ConsentRequirement : InvertableInteractionRequirement
-{
-    [DataField]
-    public ProtoId<ConsentTogglePrototype> Consent;
-
-    /// <summary>
-    ///      Whether to bypass the consent checks when interacting with yourself.
-    /// </summary>
-    [DataField]
-    public bool BypassSelf = true;
-
-    public override bool IsMet(InteractionArgs args, InteractionVerbPrototype proto, InteractionAction.VerbDependencies deps)
-    {
-        if (BypassSelf && args.User == args.Target)
-            return true;
-
-        var consent = deps.System<SharedConsentSystem>();
-        return consent.HasConsent(args.Target, Consent) ^ Inverted;
-    }
-}
