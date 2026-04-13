0dc899c73d9acd7583fa3426da2597cde37d105b
Mnemotechnician <69920617+mnemotechnician@users.noreply.github.com>
2026-04-13 12:10:44 +0300

    Fix sandbox violation

--- Content.Shared/_Floof/Humanoid/ModifyUndies/ModifyUndiesComponent.cs
+++ Content.Shared/_Floof/Humanoid/ModifyUndies/ModifyUndiesComponent.cs
@@ -10,10 +10,11 @@ public sealed partial class ModifyUndiesComponent : Component
     ///     The bodypart target enums for the undies.
     /// </summary>
     public List<HumanoidVisualLayers> BodyPartTargets =
-    [
-        HumanoidVisualLayers.UndergarmentBottom,
-        HumanoidVisualLayers.UndergarmentTop
-    ];
+        new()
+        {
+            HumanoidVisualLayers.UndergarmentBottom,
+            HumanoidVisualLayers.UndergarmentTop
+        };
 }
 
 
