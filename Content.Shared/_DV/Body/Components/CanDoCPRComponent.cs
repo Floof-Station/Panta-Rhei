c985e7815bda5104f877fb2b9cbd6986fc26850e
Raikyr0 <Kurohana@hotmail.com.au>
2026-05-02 08:48:12 +1000

    Update CanDoCPRComponent.cs
    Signed-off-by: Raikyr0 <Kurohana@hotmail.com.au>

--- Content.Shared/_DV/Body/Components/CanDoCPRComponent.cs
+++ Content.Shared/_DV/Body/Components/CanDoCPRComponent.cs
@@ -13,5 +13,30 @@ public sealed partial class CanDoCPRComponent : Component
     /// This decides when the CPR starts to work (After the first Do-After), as well as the frequency of popups.
     /// </summary>
     [DataField, AutoNetworkedField]
-    public float TimeLength = 10f;
+    public float TimeLength = 3f;
+
+    /// <summary>
+    /// The noise of CPR.
+    /// </summary>
+    [DataField]
+    public SoundSpecifier CPRSound = new SoundPathSpecifier("/Audio/Effects/CPR.ogg");
+
+    /// <summary>
+    /// The damage CPR heals.
+    /// </summary>
+    [DataField] public DamageSpecifier CPRHealing = new()
+    {
+        DamageDict =
+        {
+            ["Asphyxiation"] = -6
+        }
+    };
+    /// <summary>
+    /// The revival chance of CPR.
+    /// </summary>
+    [DataField] public float ResuscitationChance = 0.1f;
+
+    [DataField] public float RotReductionMultiplier;
+
+    public EntityUid? CPRPlayingStream;
 }
