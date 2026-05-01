f00130a771813cb83ee8248ee64920638ca21c09
Raikyr0 <Kurohana@hotmail.com.au>
2026-05-02 09:31:08 +1000

    Changed some shit to make it work
    Signed-off-by: Raikyr0 <Kurohana@hotmail.com.au>

--- Content.Shared/_DV/Body/Systems/CPRSystem.cs
+++ Content.Shared/_DV/Body/Systems/CPRSystem.cs
@@ -53,6 +53,33 @@ public sealed class CPRSystem : EntitySystem
         if (!ev.Target.HasValue || ev.Handled)
             return;
 
+        // Euphoria Start
+        if (ev.Cancelled || ev.Handled || !ev.Target.HasValue)
+        {
+            entity.Comp.CPRPlayingStream = _audio.Stop(entity.Comp.CPRPlayingStream);
+            return;
+        }
+        if (!entity.Comp.CPRHealing.Empty)
+            _damageable.TryChangeDamage(args.Target, entity.Comp.CPRHealing, true, origin: entity);
+
+        if (entity.Comp.RotReductionMultiplier > 0)
+            _rottingSystem.ReduceAccumulator(
+                (EntityUid)args.Target, entity.Comp.TimeLength * entity.Comp.RotReductionMultiplier);
+
+        if (_robustRandom.Prob(entity.Comp.ResuscitationChance)
+            && !HasComp<UnrevivableComponent>(args.Target.Value) // Floofstation - unrevivable
+            && _mobThreshold.TryGetThresholdForState((EntityUid)args.Target, MobState.Dead, out var threshold)
+            && TryComp<DamageableComponent>(args.Target, out var damageableComponent)
+            && TryComp<MobStateComponent>(args.Target, out var state)
+            && damageableComponent.TotalDamage < threshold)
+            _mobStateSystem.ChangeMobState(args.Target.Value, MobState.Critical, state, entity);
+
+        var isAlive = _mobStateSystem.IsAlive(args.Target.Value);
+        args.Repeat = !isAlive;
+        if (isAlive)
+            entity.Comp.CPRPlayingStream = _audio.Stop(entity.Comp.CPRPlayingStream);
+        // Euphoria End
+
         if (!ev.Cancelled && !_mobStateSystem.IsAlive(ev.Target.Value)) // Allow CPR on Criticial and Dead patients.
         {
             var comp = EnsureComp<AffectedByCPRComponent>(ev.Target.Value); // Enables the Crit Patient to breathe.
@@ -82,11 +109,11 @@ public sealed class CPRSystem : EntitySystem
         // Start Euphoria - These line means they need their outer clothes and mouths unblocked to receive CPR
         if (_inventory.TryGetSlotEntity(target, "outerClothing", out var outer))
         {
-            _popupSystem.PopupEntity(Loc.GetString("cpr-must-remove", ("clothing", outer)), performer, performer);
+            _popupSystem.PopupEntity(Loc.GetString("cpr-must-remove", ("clothing", outer)), user, user);
             return;
         }
 
-        if (_foodSystem.IsMouthBlocked(performer, performer) || _foodSystem.IsMouthBlocked(target, performer))
+        if (_foodSystem.IsMouthBlocked(user, user) || _foodSystem.IsMouthBlocked(target, user))
             return;
         // End Euphoria 
 
@@ -102,12 +129,12 @@ public sealed class CPRSystem : EntitySystem
         // Start Euphoria - CPR noises
         _doAfterSystem.TryStartDoAfter(doAfterArgs);
 
-        performer.Comp.CPRPlayingStream = _audio.Stop(performer.Comp.CPRPlayingStream); // Floofstation - fix any previous CPR sounds
-        var playingStream = _audio.PlayPvs(performer.Comp.CPRSound, performer, AudioParams.Default.WithLoop(true));
+        user.Comp.CPRPlayingStream = _audio.Stop(user.Comp.CPRPlayingStream); // Floofstation - fix any previous CPR sounds
+        var playingStream = _audio.PlayPvs(user.Comp.CPRSound, user, AudioParams.Default.WithLoop(true));
         if (!playingStream.HasValue)
             return;
 
-        performer.Comp.CPRPlayingStream = playingStream.Value.Entity;
+        user.Comp.CPRPlayingStream = playingStream.Value.Entity;
         // End Euphoria
 
         AddComp<AffectedByCPRComponent>(target);
@@ -140,34 +167,4 @@ public sealed class CPRSystem : EntitySystem
 
         ev.Verbs.Add(verb);
     }
-    // Start Euphoria - Do after
-    private void OnCPRDoAfter(Entity<CanDoCPRComponent> performer, ref CPRFinishedEvent args)
-    {
-        if (args.Cancelled || args.Handled || !args.Target.HasValue)
-        {
-            performer.Comp.CPRPlayingStream = _audio.Stop(performer.Comp.CPRPlayingStream);
-            return;
-        }
-
-        if (!performer.Comp.CPRHealing.Empty)
-            _damageable.TryChangeDamage(args.Target, performer.Comp.CPRHealing, true, origin: performer);
-
-        if (performer.Comp.RotReductionMultiplier > 0)
-            _rottingSystem.ReduceAccumulator(
-                (EntityUid)args.Target, performer.Comp.TimeLength * performer.Comp.RotReductionMultiplier);
-
-        if (_robustRandom.Prob(performer.Comp.ResuscitationChance)
-            && !HasComp<UnrevivableComponent>(args.Target.Value) // Floofstation - unrevivable
-            && _mobThreshold.TryGetThresholdForState((EntityUid)args.Target, MobState.Dead, out var threshold)
-            && TryComp<DamageableComponent>(args.Target, out var damageableComponent)
-            && TryComp<MobStateComponent>(args.Target, out var state)
-            && damageableComponent.TotalDamage < threshold)
-            _mobStateSystem.ChangeMobState(args.Target.Value, MobState.Critical, state, performer);
-
-        var isAlive = _mobStateSystem.IsAlive(args.Target.Value);
-        args.Repeat = !isAlive;
-        if (isAlive)
-            performer.Comp.CPRPlayingStream = _audio.Stop(performer.Comp.CPRPlayingStream);
-    }
-    // End Euphoria 
 }
