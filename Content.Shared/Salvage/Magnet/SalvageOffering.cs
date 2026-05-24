931a5379214d3ed137692cb26b87b8df9af071d5
Raikyr0 <Kurohana@hotmail.com.au>
2026-05-24 11:42:48 +1000

    Update SalvageOffering.cs
    Signed-off-by: Raikyr0 <Kurohana@hotmail.com.au>

--- Content.Shared/Salvage/Magnet/SalvageOffering.cs
+++ Content.Shared/Salvage/Magnet/SalvageOffering.cs
@@ -7,5 +7,5 @@ public record struct SalvageOffering : ISalvageMagnetOffering
 {
     public SalvageMapPrototype SalvageMap;
 
-    uint ISalvageMagnetOffering.Cost => 500; // DeltaV: Station gets next to no benefit from you pulling wrecks, force you to mine first.
+    uint ISalvageMagnetOffering.Cost => 50; // DeltaV: Station gets next to no benefit from you pulling wrecks, force you to mine first. // Euphoria: wrecks are fucking worthless most pulls
 }
