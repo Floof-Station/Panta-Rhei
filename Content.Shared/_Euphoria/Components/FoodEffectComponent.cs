fa479ea0ec420d6ab14bc88e617fc4925b9b05f4
SyaoranFox <syaoranfox@gmail.com>
2026-06-29 22:32:52 +1000

    Remove test fluids from bread. Add FoodEffectComponent to allow effects to be applied to the eater Add FoodEffect Speed to bread still need to add the actual calculations

---
+++ Content.Shared/_Euphoria/Components/FoodEffectComponent.cs
@@ -0,0 +1,30 @@
+﻿namespace Content.Shared._Euphoria.Components;
+
+[RegisterComponent]
+public sealed partial class FoodEffectComponent : Component
+{
+    [DataField]
+    public StatusEffects Effect = StatusEffects.Speed;
+
+    [DataField]
+    public TimeSpan? Time = TimeSpan.FromSeconds(2);
+
+    [DataField]
+    public StatusEffectMetabolismMode Mode = StatusEffectMetabolismMode.Update;
+
+    [DataField]
+    public TimeSpan Delay;
+}
+
+public enum StatusEffects
+{
+    Speed,
+}
+
+public enum StatusEffectMetabolismMode
+{
+    Update,
+    Add,
+    Remove,
+    Set,
+}
