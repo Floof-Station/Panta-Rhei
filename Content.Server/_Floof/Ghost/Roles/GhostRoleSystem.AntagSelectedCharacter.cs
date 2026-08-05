2717b0fa6bbdcdeb80ca32193c0ca52e231b5741
Sydney Grayy <sydneygray64@protonmail.com>
2026-08-04 17:57:16 -0700

    Remove the useless call that broke everything occasionally

--- Content.Server/_Floof/Ghost/Roles/GhostRoleSystem.AntagSelectedCharacter.cs
+++ Content.Server/_Floof/Ghost/Roles/GhostRoleSystem.AntagSelectedCharacter.cs
@@ -77,14 +77,6 @@ namespace Content.Server.Ghost.Roles
 
             EnsureComp<MindContainerComponent>(args.Entity.Value);
 
-	    /*
-	    This call occasionally throws an error and freezes the client until they manually ghost again
-	    when attempting to take the ghost role. This is because it's attempting to get a
-	    GhostRoleComponent on the gamerule, which does not exist on the gamerule.
-	    This only happens *sometimes*, not often, and I do not know why.
-	    */
-	    GhostRoleInternalCreateMindAndTransfer(args.Session, uid, args.Entity.Value);
-
             _outfit.SetOutfit(args.Entity.Value, component.OutfitPrototype);
 
             EntityManager.AddComponents(args.Entity.Value, component.AddedComponents);
