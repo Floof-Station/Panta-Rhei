bd3759162e2194cee6a12f9d61adbbd3fe1e16a9
Mnemotechnician <69920617+mnemotechnician@users.noreply.github.com>
2026-08-28 08:14:00 +0300

    Also add a sanity check and support for PolygonShape

--- Content.Shared/_Floof/HeightAdjust/FixtureHelperSystem.cs
+++ Content.Shared/_Floof/HeightAdjust/FixtureHelperSystem.cs
@@ -24,10 +24,18 @@ public class FixtureHelperSystem : EntitySystem
             return 0;
 
         var count = 0;
+        var uniqueShapes = new HashSet<IPhysShape>();
         foreach (var (key, fix) in ent.Comp.Fixtures)
         {
             if (fix.Shape is not PhysShapeCircle circle || circle.Radius <= 0.01f)
                 continue;
+
+            // Sanity check
+            if (!uniqueShapes.Add(fix.Shape))
+            {
+                Log.Warning($"Entity {ToPrettyString(ent)} has two or more fixtures that reference the same IPhysShape object. This can cause errors.");
+                continue;
+            }
 
             // Can we avoid the costly SetRadius in batch fixture updates like this?
             // Setting fixture.Radius and calling FixtureUpdate would be an option, but it's internal API
@@ -45,6 +53,8 @@ public class FixtureHelperSystem : EntitySystem
             PhysShapeCircle circle => new PhysShapeCircle(circle.Radius, circle.Position),
             // There is no way to set the fields of PhysShapeAABB from code, I'm not even kidding
             PhysShapeAabb aab => _serialization.CreateCopy(aab, notNullableOverride: true),
+            // Same here
+            PolygonShape polygon => _serialization.CreateCopy(polygon, notNullableOverride: true),
             _ => null,
         };
 
