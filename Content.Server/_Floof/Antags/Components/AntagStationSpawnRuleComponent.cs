0780a66bee9a7444cc937ccf237e00bdcfe60e36
Sydney Grayy <sydneygray64@protonmail.com>
2026-09-15 16:25:17 -0700

    Added a single word in this one summary

--- Content.Server/_Floof/Antags/Components/AntagStationSpawnRuleComponent.cs
+++ Content.Server/_Floof/Antags/Components/AntagStationSpawnRuleComponent.cs
@@ -4,7 +4,7 @@ namespace Content.Server.Antag.Components;
 
 /// <summary>
 /// Spawns this rule's antags on a station. It will attempt to
-/// obtain and return the coordinates for spawnpoints and choose one at random.
+/// obtain and return the coordinates for latejoin spawnpoints and choose one at random.
 /// Failing that, it will attempt to obtain any tile on a station.
 /// Requires <see cref="AntagSelectionComponent"/>.
 /// </summary>
