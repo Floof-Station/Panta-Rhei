09b3678c0fdcb220f091c3b2a72b29d15d7b6096
M3739 <47579354+M3739@users.noreply.github.com>
2026-09-18 16:51:11 -0400

    Proofread comment #1
    Signed-off-by: M3739 <47579354+M3739@users.noreply.github.com>

--- Content.Server/_DV/Curation/Systems/CwoinkSystem.cs
+++ Content.Server/_DV/Curation/Systems/CwoinkSystem.cs
@@ -40,7 +40,7 @@ public sealed partial class CwoinkSystem : SharedCwoinkSystem
 
     [Dependency] private readonly IPlayerManager _playerManager = default!;
     [Dependency] private readonly IAdminManager _adminManager = default!;
-   // [Dependency] private readonly IBanManager _banManager = default!; // Starlight //Euphoia, Unused.
+   // [Dependency] private readonly IBanManager _banManager = default!; // Starlight //Euphoria, Unused.
     [Dependency] private readonly IConfigurationManager _config = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly IPlayerLocator _playerLocator = default!;
