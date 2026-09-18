4386c54ad52c4afffc8e148265c2ed14e22480fd
cyberjoel <cyberjoel@hotmail.com>
2026-09-18 14:37:14 -0400

    Changing the cooldown, and disabling auto ban for spam.

--- Content.Server/_DV/Curation/Systems/CwoinkSystem.cs
+++ Content.Server/_DV/Curation/Systems/CwoinkSystem.cs
@@ -79,7 +79,7 @@ public sealed partial class CwoinkSystem : SharedCwoinkSystem
     // Should be shorter than DescriptionMax
     private const ushort MessageLengthCap = 3000;
 
-    private readonly TimeSpan _messageCooldown = TimeSpan.FromSeconds(2);
+    private readonly TimeSpan _messageCooldown = TimeSpan.FromSeconds(0); //Euphoria, Everyone is getting tired of dropped messages cause of this.
 
     private readonly Queue<(NetUserId Channel, string Text, TimeSpan Timestamp)> _recentMessages = new();
     private const int MaxRecentMessages = 10;
@@ -728,8 +728,9 @@ public sealed partial class CwoinkSystem : SharedCwoinkSystem
         if (IsOnCooldown(message.UserId, currentTime))
             return;
 
-        if (IsSpam(message.UserId, message.Text))
-            _banManager.CreateServerBan(senderSession.UserId, senderSession.Name, null, null, null, 0, NoteSeverity.High, "Automatic AHELP Antispam system Ban, If this ban is wrong, file an appeal.");
+        //if (IsSpam(message.UserId, message.Text))
+            //_banManager.CreateServerBan(senderSession.UserId, senderSession.Name, null, null, null, 0, NoteSeverity.High, "Automatic AHELP Antispam system Ban, If this ban is wrong, file an appeal.");
+            //Euphoria, Since we are a invite only server we dont need a autoban for raiders.
 
         AddToRecentMessages(message.UserId, message.Text, currentTime);
         // End Starlight Changes
